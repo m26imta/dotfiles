d9cfb397f78aa524a373bd8a18d3a373c786b757
m26 <lslocal@mail>
2023-11-02 04:38:06 +0700

    remove .config/nvim

--- .config/nvim/lua/core/init.lua
+++
@@ -1,3 +0,0 @@
-require("core.autocmd")
-require("core.options")
-require("core.keymaps")
--- .config/nvim/lua/plugins/dependencies.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- dependencies
-  {"nvim-lua/plenary.nvim", lazy = true},
-  {"nvim-tree/nvim-web-devicons", lazy = true},
-  {"MunifTanjim/nui.nvim", lazy = true},
-}
