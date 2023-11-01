cc3ef3abea48352004f3a71f005f889d908728ce
m26 <m26@local>
2023-11-01 14:53:44 -0400

    add some config

---
+++ scripts/font-install.sh
@@ -0,0 +1,6 @@
+[[ ! -d ~/.local/share/fonts ]] && mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFont-Regular.ttf
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Agave/AgaveNerdFont-Regular.ttf
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFont-Regular.ttf
+
