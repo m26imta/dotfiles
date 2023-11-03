54b9c8ae6a8e09288a865a93d74a9111ea1f62ed
m26 <lslocal@mail>
2023-11-03 07:20:32 +0700

    add theme vscode

--- m26nvv/nvim/lua/plugins/themes.lua
+++ m26nvv/nvim/lua/plugins/themes.lua
@@ -1,5 +1,5 @@
 local theme = "nightfox/carbonfox"
--- "nightfox/carbonfox"  OR  "nightfox"
+-- "gruvboxm/gruvbox-material"  OR  "gruvboxm"
 
 local preinstall = false    -- true: install all themes / false: install only 1 theme
 local islazy = true  -- themes is lazy, so use :Lazy load foo.nvim to invoke and then use :color ... to set colorscheme
@@ -12,17 +12,18 @@ local builtin_themes = {"blue", "darkblue", "default", "delek", "desert", "elflo
   "pablo", "peachpuff", "quiet", "ron", "shine", "slate", "torte", "zellner"}
 
 local themes = {
-  { "joshdick/onedark.vim",           name = "onedark",          varients = { "onedark" } },
-  { "lunarvim/darkplus.nvim",         name = "darkplus",         varients = { "darkplus" } },
-  { "bluz71/vim-nightfly-guicolors",  name = "nightfly",         varients = { "nightfly" } },
-  { "sainnhe/everforest",             name = "everforest",       varients = { "everforest" } },
-  { "morhetz/gruvbox",                name = "gruvbox",          varients = { "gruvbox" } },
-  { "sainnhe/gruvbox-material",       name = "gruvbox-material", varients = { "gruvbox-material" } },
-  { "Mofiqul/dracula.nvim",           name = "dracula",          varients = { "dracula", "dracula-soft" } },
-  { "EdenEast/nightfox.nvim",         name = "nightfox",         varients = { "nightfox", "dayfox", "dawnfox", "duskfox", "nordfox", "terafox", "carbonfox"} },
-  { "folke/tokyonight.nvim",          name = "tokyonight",       varients = { "tokyonight", "tokyonight-day", "tokyonight-moon", "tokyonight-night", "tokyonight-storm" } },
-  { "catppuccin/nvim",                name = "catppuccin",       varients = { "catppuccin", "catppuccin-frappe", "catppuccin-latte", "catppuccin-macchiato", "catppuccin-mocha" } },
-  { "projekt0n/github-nvim-theme",    name = "github-theme",     varients = { "github_dark", "github_dark_colorblind", "github_dark_default", "github_dark_dimmed", "github_dark_high_contrast", "github_dark_tritanopia", "github_dimmed", "github_light", "github_light_colorblind", "github_light_default", "github_light_high_contrast", "github_light_tritanopia" } },
+  { "joshdick/onedark.vim",           name = "onedark",            varients = { "onedark" } },
+  { "lunarvim/darkplus.nvim",         name = "darkplus",           varients = { "darkplus" } },
+  { "bluz71/vim-nightfly-guicolors",  name = "nightfly",           varients = { "nightfly" } },
+  { "sainnhe/everforest",             name = "everforest",         varients = { "everforest" } },
+  { "morhetz/gruvbox",                name = "gruvbox",            varients = { "gruvbox" } },
+  { "sainnhe/gruvbox-material",       name = "gruvboxm",           varients = { "gruvbox-material" } },
+  { "askfiy/visual_studio_code",      name = "vscode",             varients = { "visual_studio_code" } },
+  { "Mofiqul/dracula.nvim",           name = "dracula",            varients = { "dracula", "dracula-soft" } },
+  { "EdenEast/nightfox.nvim",         name = "nightfox",           varients = { "nightfox", "dayfox", "dawnfox", "duskfox", "nordfox", "terafox", "carbonfox"} },
+  { "folke/tokyonight.nvim",          name = "tokyonight",         varients = { "tokyonight", "tokyonight-day", "tokyonight-moon", "tokyonight-night", "tokyonight-storm" } },
+  { "catppuccin/nvim",                name = "catppuccin",         varients = { "catppuccin", "catppuccin-frappe", "catppuccin-latte", "catppuccin-macchiato", "catppuccin-mocha" } },
+  { "projekt0n/github-nvim-theme",    name = "github",             varients = { "github_dark", "github_dark_colorblind", "github_dark_default", "github_dark_dimmed", "github_dark_high_contrast", "github_dark_tritanopia", "github_dimmed", "github_light", "github_light_colorblind", "github_light_default", "github_light_high_contrast", "github_light_tritanopia" } },
 }
 
 local t = {}
